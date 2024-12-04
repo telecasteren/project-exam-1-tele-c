84ea01966be9c1831b4781532636470b7377216f
telecasteren <127958662+telecasteren@users.noreply.github.com>
2024-12-04 22:41:13 +0100

    .

--- js/app/ui/components/colorMode/setSpecificColors.js
+++ js/app/ui/components/colorMode/setSpecificColors.js
@@ -1,16 +1,16 @@
 export function setSpecificColors() {
   const submitBtn = document.querySelector("#submitBtn");
+  const navLinks = document.querySelectorAll(".navLinks");
   const currentMode = window.localStorage.getItem("colorMode");
 
   if (!submitBtn) {
     console.error("Couldn't set unique color, submit button not found");
-    return;
-  }
-
-  // If color mode rb and br is true, set unique color on submit button
-  if (currentMode === "rb" || currentMode === "br") {
-    submitBtn.style.backgroundColor = "var(--tertiary-color)";
   } else {
-    submitBtn.style.backgroundColor = "";
+    // Set submit button color based on if rb or br is colorMode
+    if (currentMode === "rb" || currentMode === "br") {
+      submitBtn.style.backgroundColor = "var(--tertiary-color)";
+    } else {
+      submitBtn.style.backgroundColor = "";
+    }
   }
 }
