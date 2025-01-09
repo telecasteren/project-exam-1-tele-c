ed8b2e091908fef6d6e40b084bda6b36d57cbe8a
telecasteren <127958662+telecasteren@users.noreply.github.com>
2025-01-09 20:15:09 +0100

    fixed index path to post

--- js/app/eventListeners/goToPost.js
+++ js/app/eventListeners/goToPost.js
@@ -1,6 +1,6 @@
 // Redirect to specific posts
 export function goToPost(postID) {
   if (postID) {
-    window.location.href = `/blog/post/index.html?postId=${postID}`;
+    window.location.href = `/blog/post/?postId=${postID}`;
   }
 }
