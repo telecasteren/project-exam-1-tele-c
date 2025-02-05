c292e4c182162239b0bad3df3c45e96467762665
telecasteren <127958662+telecasteren@users.noreply.github.com>
2025-02-05 18:10:22 +0100

    responsive adjustment titleBlob for iOS and smaller screens

--- js/app/ui/blogs/thumbnails/thumbnails.js
+++ js/app/ui/blogs/thumbnails/thumbnails.js
@@ -5,16 +5,23 @@ export function createThumbnails(post) {
   thumbnails.dataset.publishDate = post.publishDate;
   thumbnails.dataset.category = post.postCategory;
 
-  const titleBlob = document.createElement("p");
-  titleBlob.classList.add("titleBlob");
-  titleBlob.innerText = post.title;
+  // const titleBlob = document.createElement("p");
+  // titleBlob.classList.add("titleBlob");
+  // titleBlob.innerText = post.title;
 
   const thumbImg = document.createElement("img");
   thumbImg.classList.add("thumbImg");
   thumbImg.src = post.imgSrc;
   thumbImg.alt = post.imgAlt;
 
-  thumbnails.appendChild(titleBlob);
+  // const thumbnailContainer = document.querySelectorAll(".thumb-container");
+  // if (thumbnailContainer) {
+  //   thumbnailContainer.forEach((container) => {
+  //     container.appendChild(titleBlob);
+  //   });
+  // }
+
+  // thumbnails.appendChild(titleBlob);
   thumbnails.appendChild(thumbImg);
 
   return thumbnails;
